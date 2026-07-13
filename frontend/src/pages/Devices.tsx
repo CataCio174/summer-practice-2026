60b5fe485aac1769d2c012ef0b49b81a8652cac8
Hotea Emanuel <emanuelhotea1@gmail.com>
2026-07-13 22:43:30 +0300

    feat:device removing in front

--- frontend/src/pages/Devices.tsx
+++ frontend/src/pages/Devices.tsx
@@ -1,201 +1,224 @@
 import { useState, useEffect, useMemo } from "react";
 import {
-    MenuItem,
-    Dialog,
-    DialogTitle,
-    DialogContent,
-    DialogActions,
-    Button,
-    Container,
-    Typography,
-    Divider,
-    Box,
+  MenuItem,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  Container,
+  Typography,
+  Divider,
+  Box,
 } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
-import { MaterialReactTable, useMaterialReactTable } from "material-react-table";
+import {
+  MaterialReactTable,
+  useMaterialReactTable,
+} from "material-react-table";
 import AddDeviceForm from "../components/AddDeviceForm";
 import PageHeader from "../components/PageHeader";
 
+type ActionType = "schedule" | "edit" | "remove" | null;
+
 const DeviceTable = () => {
-    const [devices, setDevices] = useState([]);
-    const [selectedDevice, setSelectedDevice] = useState(null);
-    const [isScheduleDialogOpen, setIsScheduleDialogOpen] = useState(false);
-    const [selectedAction, setSelectedAction] = useState(null);
-    const [isAddDialogOpen, setIsAddDialogOpen] = useState(false);
+  const [devices, setDevices] = useState([]);
+  const [selectedDevice, setSelectedDevice] = useState(null);
+  const [isScheduleDialogOpen, setIsScheduleDialogOpen] = useState(false);
+  const [selectedAction, setSelectedAction] = useState(null);
+  const [isAddDialogOpen, setIsAddDialogOpen] = useState(false);
 
-    const fetchDevices = async () => {
-        try {
-            const response = await fetch("/api/devices");
-            if (!response.ok) {
-                throw new Error(`Failed to fetch devices: ${response.status}`);
-            }
-            const data = await response.json();
-            setDevices(data);
-        } catch (error) {
-            console.error("Error fetching devices:", error);
-        }
-    };
+  const fetchDevices = async () => {
+    try {
+      const response = await fetch("/api/devices");
+      if (!response.ok) {
+        throw new Error(`Failed to fetch devices: ${response.status}`);
+      }
+      const data = await response.json();
+      setDevices(data);
+    } catch (error) {
+      console.error("Error fetching devices:", error);
+    }
+  };
 
-    useEffect(() => {
-        fetchDevices();
-    }, []);
+  useEffect(() => {
+    fetchDevices();
+  }, []);
 
-    const handleScheduleOpen = (device) => {
-        setSelectedDevice(device);
-        setIsScheduleDialogOpen(true);
-        setSelectedAction("schedule");
-    };
+  const handleScheduleOpen = (device) => {
+    setSelectedDevice(device);
+    setIsScheduleDialogOpen(true);
+    setSelectedAction("schedule");
+  };
 
-    const handleScheduleClose = () => {
-        setIsScheduleDialogOpen(false);
-        setSelectedAction(null);
-        setSelectedDevice(null);
-    };
+  const handleScheduleClose = () => {
+    setIsScheduleDialogOpen(false);
+    setSelectedAction(null);
+    setSelectedDevice(null);
+  };
 
-    const handleAction = (action, device) => {
-        switch (action) {
-            case "schedule":
-                handleScheduleOpen(device);
-                break;
-            case "edit":
-                // Handle edit action
-                break;
-            case "remove":
-                // Handle remove action
-                break;
-            default:
-                break;
-        }
-    };
+  const handleRemoving = async (id: string) => {
+    try {
+      const response = await fetch("/api/device/" + id, {
+        method: "DELETE",
+      });
+      if (!response.ok) {
+        throw new Error(`Failed to fetch devices: ${response.status}`);
+      }
+      await response.json();
+      setDevices(devices.filter((device) => device._id.$oid !== id));
+    } catch (error) {
+      console.error("Error fetching devices:", error);
+    }
+  };
 
-    const columns = useMemo(
-        () => [
-            { accessorKey: "deviceName", header: "Device Name" },
-            { accessorKey: "deviceSlNo", header: "Serial Number" },
-            { accessorKey: "deviceType", header: "Device Type" },
-            { accessorKey: "hwType", header: "Hardware Type" },
-            { accessorKey: "site", header: "Site" },
-            { accessorKey: "group", header: "Group" },
-            { accessorKey: "owner", header: "Owner" },
-            {
-                id: "connection",
-                header: "Connection",
-                accessorFn: (row) => {
-                    const type = row.connectivityType || "-";
-                    const ip = row.ip || "-";
-                    const port = row.port || "-";
-                    return `${type} | ${ip}:${port}`;
-                },
-            },
-        ],
-        [],
-    );
+  const handleAction = (action: ActionType, device: any) => {
+    switch (action) {
+      case "schedule":
+        handleScheduleOpen(device);
+        break;
+      case "edit":
+        // Handle edit action
+        break;
+      case "remove":
+        handleRemoving(device._id.$oid);
+        break;
+      default:
+        break;
+    }
+  };
 
-    const table = useMaterialReactTable({
-        columns,
-        data: devices,
-        enableRowActions: true,
-        positionActionsColumn: "last",
-        muiTableContainerProps: {
+  const columns = useMemo(
+    () => [
+      { accessorKey: "deviceName", header: "Device Name" },
+      { accessorKey: "deviceSlNo", header: "Serial Number" },
+      { accessorKey: "deviceType", header: "Device Type" },
+      { accessorKey: "hwType", header: "Hardware Type" },
+      { accessorKey: "site", header: "Site" },
+      { accessorKey: "group", header: "Group" },
+      { accessorKey: "owner", header: "Owner" },
+      {
+        id: "connection",
+        header: "Connection",
+        accessorFn: (row) => {
+          const type = row.connectivityType || "-";
+          const ip = row.ip || "-";
+          const port = row.port || "-";
+          return `${type} | ${ip}:${port}`;
         },
-        muiTablePaperProps: {
-            elevation: 0,
-        },
-        renderTopToolbarCustomActions: () => (
-            <Button variant="outlined" startIcon={<AddIcon />} onClick={handleAddDialogOpen}>
-                Add Device
-            </Button>
-        ),
-        renderRowActionMenuItems: ({ row, closeMenu }) => [
-            <MenuItem
-                key="schedule"
-                onClick={() => {
-                    handleAction("schedule", row.original);
-                    closeMenu();
-                }}
-            >
-                Schedule
-            </MenuItem>,
-            <MenuItem
-                key="edit"
-                onClick={() => {
-                    handleAction("edit", row.original);
-                    closeMenu();
-                }}
-            >
-                Edit
-            </MenuItem>,
-            <MenuItem
-                key="remove"
-                onClick={() => {
-                    handleAction("remove", row.original);
-                    closeMenu();
-                }}
-            >
-                Remove
-            </MenuItem>,
-        ],
-    });
+      },
+    ],
+    [],
+  );
 
-    const handlePerformAction = async () => {
-        if (!selectedDevice) return;
-        try {
-            switch (selectedAction) {
-                case "schedule":
-                    // Perform schedule action with selectedDevice._id
-                    console.log(`Scheduled action for device ${selectedDevice._id}`);
-                    break;
-                case "edit":
-                    // Perform edit action with selectedDevice._id
-                    break;
-                case "remove":
-                    // Perform remove action with selectedDevice._id
-                    break;
-                default:
-                    break;
-            }
-            handleScheduleClose();
-        } catch (error) {
-            console.error("Error performing action:", error);
-        }
-    };
+  const table = useMaterialReactTable({
+    columns,
+    data: devices,
+    enableRowActions: true,
+    positionActionsColumn: "last",
+    muiTableContainerProps: {},
+    muiTablePaperProps: {
+      elevation: 0,
+    },
+    renderTopToolbarCustomActions: () => (
+      <Button
+        variant="outlined"
+        startIcon={<AddIcon />}
+        onClick={handleAddDialogOpen}
+      >
+        Add Device
+      </Button>
+    ),
+    renderRowActionMenuItems: ({ row, closeMenu }) => [
+      <MenuItem
+        key="schedule"
+        onClick={() => {
+          handleAction("schedule", row.original);
+          closeMenu();
+        }}
+      >
+        Schedule
+      </MenuItem>,
+      <MenuItem
+        key="edit"
+        onClick={() => {
+          handleAction("edit", row.original);
+          closeMenu();
+        }}
+      >
+        Edit
+      </MenuItem>,
+      <MenuItem
+        key="remove"
+        onClick={() => {
+          handleAction("remove", row.original);
+          closeMenu();
+        }}
+      >
+        Remove
+      </MenuItem>,
+    ],
+  });
 
-    const handleAddDialogOpen = () => {
-        setIsAddDialogOpen(true);
-    };
+  const handlePerformAction = async () => {
+    if (!selectedDevice) return;
+    try {
+      switch (selectedAction) {
+        case "schedule":
+          // Perform schedule action with selectedDevice._id
+          console.log(`Scheduled action for device ${selectedDevice._id}`);
+          break;
+        case "edit":
+          // Perform edit action with selectedDevice._id
+          break;
+        case "remove":
+          // Perform remove action with selectedDevice._id
+          break;
+        default:
+          break;
+      }
+      handleScheduleClose();
+    } catch (error) {
+      console.error("Error performing action:", error);
+    }
+  };
 
-    const handleAddDialogClose = () => {
-        setIsAddDialogOpen(false);
-    };
+  const handleAddDialogOpen = () => {
+    setIsAddDialogOpen(true);
+  };
 
-    const handleAddDeviceSuccess = () => {
-        fetchDevices();
-    };
+  const handleAddDialogClose = () => {
+    setIsAddDialogOpen(false);
+  };
 
-    return (
-        <Container maxWidth={false} disableGutters>
-            <PageHeader title="Devices" breadcrumbItems={["Home", "Devices"]} />
-            <MaterialReactTable table={table} />
-            <Dialog open={isScheduleDialogOpen} onClose={handleScheduleClose}>
-                <DialogTitle>Schedule Action</DialogTitle>
-                <DialogContent>
-                    {/* Add content for scheduling here */}
-                    Schedule dialog content...
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleScheduleClose}>Cancel</Button>
-                    <Button onClick={handlePerformAction} color="primary">
-                        Schedule
-                    </Button>
-                </DialogActions>
-            </Dialog>
-            <AddDeviceForm
-                open={isAddDialogOpen}
-                onClose={handleAddDialogClose}
-                onSuccess={handleAddDeviceSuccess}
-            />
-        </Container>
-    );
+  const handleAddDeviceSuccess = () => {
+    fetchDevices();
+  };
+
+  return (
+    <Container maxWidth={false} disableGutters>
+      <PageHeader title="Devices" breadcrumbItems={["Home", "Devices"]} />
+      <MaterialReactTable table={table} />
+      <Dialog open={isScheduleDialogOpen} onClose={handleScheduleClose}>
+        <DialogTitle>Schedule Action</DialogTitle>
+        <DialogContent>
+          {/* Add content for scheduling here */}
+          Schedule dialog content...
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleScheduleClose}>Cancel</Button>
+          <Button onClick={handlePerformAction} color="primary">
+            Schedule
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <AddDeviceForm
+        open={isAddDialogOpen}
+        onClose={handleAddDialogClose}
+        onSuccess={handleAddDeviceSuccess}
+      />
+    </Container>
+  );
 };
 
 export default DeviceTable;
